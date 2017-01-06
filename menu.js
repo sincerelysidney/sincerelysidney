4ec4b4e52351603e8b413abb6bf311440b7179d7
Morgan Reschenberg <mreschenberg@berkeley.edu>
2017-01-06 13:59:02 -0800

    heres some things

--- menu.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    document.getElementById("menuAnchor").style.width = "200px";
-    document.getElementById("main").style.marginLeft = "200px";
-}
-
-function closeNav() {
-    document.getElementById("menuAnchor").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-}
